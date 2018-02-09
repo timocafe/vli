fd9dc3754726da445a1d2389d72b2e6bf6004f0a
timocafe <timothee.ewart@gmail.com>
2018-02-09 20:54:28 +0100

    clean up cmake and openmp issue

--- main/main.cpp
+++ main/main.cpp
@@ -2,12 +2,24 @@
 
     #include <iostream>
     #include "vli/integer.hpp"
-    #include "vli/polynomial.hpp"
-    #include "vli/vector.hpp"
 
     int main(int argc, char * argv[]){
-      vli::integer<128> a(1);
-      std::cout << std::hex << a << std::endl;
+      size_t a[120];
+      size_t b[120];
+
+      for(int i=0; i < 120; ++i){
+          a[i] = -1;
+          b[i] = 0;
+      }
+
+      b[0] = 1;
+
+      asm volatile ( "addq  %1 , %0 ; \n\t" : "+&r"(a[0]):"rm"(b[0]));
+      for(int i=1; i < 120; ++i)
+         asm volatile ( "adcq  %1 , %0 ; \n\t" : "+r"(a[i]):"rm"(b[i]));
+
+      for(int i=0; i < 120; ++i)
+        std::cout <<   a[i] << std::endl;
     }
 
 /* \endcond I do not need this part in the doc*/
